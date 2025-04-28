a2b9479b3a408a49d0d9930dd0b346fd9d7dc307
André <wojcik.andre2.0@gmail.com>
2025-04-27 22:41:27 -0400

    background change Anchoine qui cache

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ pygame.init()
 ######## SETTINGS ########
 background_colour = (0,0,0)
 background = pygame.image.load("background.jpg")
-(width, height) = (1800, 1000)
+(width, height) = (1800, 1200)
 screen_size = (width, height)
 background_image = pygame.transform.scale(background, screen_size)
 font = pygame.font.Font('freesansbold.ttf', 15)
 
 grid = (4, 4)
 nb_case = grid[0]*grid[1]
-tile_size = (width/grid[0] - width/(grid[0]+(grid[0]*4)), height/grid[1] - height/(grid[1]+(grid[1]*2.5)) )
+tile_size = (width/grid[0] - width/(grid[0]+(grid[0]*4)), height/grid[1] - height/(grid[1]+(grid[1]*2.6)) )
 cross = pygame.transform.scale(pygame.image.load("cross.png"), tile_size)
 greenCross = pygame.transform.scale(pygame.image.load("green_cross.png"), tile_size)
 print(tile_size)
@@ -74,7 +74,8 @@ def choose_target(seed):
     print("(((())))",type(seed))
     for i in range(len(seed)):
         print("############", seed[i])
-    return display( pygame.transform.scale( pygame.image.load (io.BytesIO (urlopen(images[int(seed[random.randint(0,len(seed)-1)])][1].replace(' ,', '')).read())) ,tile_size), (0,screen_size[1]-400))
+    return pygame.transform.scale( pygame.image.load (io.BytesIO (urlopen(images[int(seed[random.randint(0,len(seed)-1)])][1].replace(' ,', '')).read())) ,tile_size)
+
 
 
 ########################################
@@ -102,14 +103,19 @@ seed_image = pygame.transform.scale(pygame.image.load('copy.png'),(50,50))
 
 seek_button = pygame.Rect((0,screen_size[1]-200),(50,50))
 seek_image = pygame.transform.scale(pygame.image.load('red.png'),(50,50))
+
+show_target = pygame.transform.scale(pygame.image.load('show_button.png'),(100,70))
+show_target_rect = pygame.Rect((0,screen_size[1]-500),(100,70))
+
+image_blocker = pygame.transform.scale(pygame.image.load('Anchoine.jpg'),tile_size)
 #########################################################################   GAME LOOP   #########################################################################
 
 running = True
 while running:
-
     display(seed_display, (0,screen_size[1]-50))
     display(seed_image,(0,screen_size[1]-100))
     display(seek_image,(0,screen_size[1]-200))
+    display(show_target,(0,screen_size[1]-500))
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT or len(tiles) == 0:
@@ -127,13 +133,21 @@ while running:
                 pyperclip.copy(seed)
                 print("Copied in the Clipboard")
             elif seek_button.collidepoint(pos):
-                choose_target( pyperclip.paste().replace('[','').replace(']','').split(', ') )
+                seed_choice = choose_target( pyperclip.paste().replace('[','').replace(']','').split(', ') )
+                display(seed_choice, (0,screen_size[1]-400))
                 print(pyperclip.paste())
+            elif show_target_rect.collidepoint(pos):
+                display(image_blocker, (0,screen_size[1]-400))
+                print("Target Displayed")
         elif event.button == 3:
             pos = pygame.mouse.get_pos()  # Obtient la position de la souris
             for rect in tiles:
                 if rect[0].collidepoint(pos):  # Vérifie si le rectangle est cliqué
+                    print("Blocked")
                     display(rect[1], rect[0].topleft)
+            if show_target_rect.collidepoint(pos):
+                display( seed_choice, (0,screen_size[1]-400))
+                print("Target Shown")
         elif event.button == 2:
             pos = pygame.mouse.get_pos()
             for rect in tiles:
